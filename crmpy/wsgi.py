eca2ecd973946d77ae1e9e93f98e09b8da3cdc4d
Fahim Ahmed <devfahimahmed@gmail.com>
2021-02-25 23:27:05 +0600

    collecstatic added

--- crmpy/wsgi.py
+++ crmpy/wsgi.py
@@ -1,21 +1,13 @@
-"""
-WSGI config for crmpy project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# assuming your Django settings file is at
+# '/home/myusername/mysite/mysite/settings.py'
+path = '/home/devfahim/pypos-live'
+if path not in sys.path:
+     sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'crmpy.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crmpy.settings')
-
 application = get_wsgi_application()
-
-
-if os.getcwd() == '/app':
-    from whitenoise.django import DjangoWhiteNoise
-    application = DjangoWhiteNoise(application)     
